b7f94bbbf630f3c6bb48072b4324a2d138d4f4dd
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2023-12-08 03:01:34 +0800

    adad

--- energywise/src/main/java/com/energywise/energywise/Controller/BillController.java
+++ energywise/src/main/java/com/energywise/energywise/Controller/BillController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BillController {
 
     private final List<User> users = new ArrayList<>();
+
+    @GetMapping("/print")
+    public String printHello() {
+        return "Hello, Mic Test!";
+    }
 
     @PostMapping("/create")
     public ResponseEntity<String> createBill(@RequestBody User user) {
--- energywise/src/main/java/com/energywise/energywise/Controller/CalendarController.java
+++ energywise/src/main/java/com/energywise/energywise/Controller/CalendarController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalendarController {
 
     private final List<Calendar> events = new ArrayList<>();
+
+    @GetMapping("/print")
+    public String printHello() {
+        return "Hello, Mic Test!";
+    }
 
     @PostMapping("/create")
     public ResponseEntity<String> createEvent(@RequestBody Calendar event) {
