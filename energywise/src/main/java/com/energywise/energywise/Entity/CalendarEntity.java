9c0bab6fa5dd0ee0f598942aacc503a62270b364
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2023-12-17 03:01:15 +0800

    a

--- energywise/src/main/java/com/energywise/energywise/Entity/CalendarEntity.java
+++ energywise/src/main/java/com/energywise/energywise/Entity/CalendarEntity.java
@@ -1,47 +1,40 @@
 package com.energywise.energywise.Entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tblcalendar")
 public class CalendarEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "event_id")
-    private int eventId;
+    @Column
+    private int id;
 
-    @Column(name = "event_name")
     private String eventName;
-
-    @Column(name = "event_description")
+    private Date eventDate;
     private String eventDescription;
 
-    @Column(name = "event_date")
-    private java.sql.Date eventDate;
-
     public CalendarEntity() {
-        super();
     }
 
-    public CalendarEntity(int eventId, String eventName, String eventDescription, java.sql.Date eventDate) {
-        this.eventId = eventId;
+    public CalendarEntity(String eventName, Date eventDate, String eventDescription) {
         this.eventName = eventName;
-        this.eventDescription = eventDescription;
         this.eventDate = eventDate;
+        this.eventDescription = eventDescription;
     }
 
-    public int getEventId() {
-        return eventId;
+    public Long getId() {
+        return id;
     }
 
-    public void setEventId(int eventId) {
-        this.eventId = eventId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEventName() {
@@ -52,31 +45,19 @@ public class CalendarEntity {
         this.eventName = eventName;
     }
 
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
     public String getEventDescription() {
         return eventDescription;
     }
 
     public void setEventDescription(String eventDescription) {
         this.eventDescription = eventDescription;
-    }
-
-    public java.sql.Date getEventDate() {
-        return eventDate;
-    }
-
-    public void setEventDate(java.sql.Date eventDate) {
-        this.eventDate = eventDate;
-    }
-
-    public void addEvent() {
-    }
-
-    public void editEvent() {
-    }
-
-    public void viewEvent() {
-    }
-
-    public void deleteEvent() {
     }
 }
