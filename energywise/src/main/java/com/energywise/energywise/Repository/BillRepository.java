46c2845a932fa6ef0851605359fb2f05f13740cc
Dave Ondona <ondonadave5@gmail.com>
2023-12-18 19:45:41 +0800

    Bill is depreciated.

--- energywise/src/main/java/com/energywise/energywise/Repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energywise.energywise.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.energywise.energywise.Entity.BillEntity;
-
-public interface BillRepository extends JpaRepository<BillEntity, Integer> {
-
-}
